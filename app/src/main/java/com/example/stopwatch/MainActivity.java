53ece4caa65c8db8545773bbbd128ae93b53015d
Levitsiy <LeoLudensLoqui@gmail.com>
2020-04-13 13:05:07 +0400

    MyFirstCommit

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean isRunning;
+    private boolean wasRunning;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            isRunning = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("warRunning");
+        }
+
+        runTimer();
+    }
+
+    public void onClickStart(View view) {
+        isRunning = true;
+    }
+
+    public void onClickStop(View view) {
+        isRunning = false;
+    }
+
+    public void onClickReset(View view) {
+        isRunning = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+        final TextView stopwatch = findViewById(R.id.stopwatch);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/60;
+                int minutes = (seconds % 3600) / 60;
+                int sec = seconds % 60;
+
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, sec);
+                stopwatch.setText(time);
+
+                if (isRunning) {
+                    seconds++;
+                }
+
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", isRunning);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        wasRunning = isRunning;
+        isRunning = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (wasRunning) {
+            isRunning = true;
+        }
+    }
+}
